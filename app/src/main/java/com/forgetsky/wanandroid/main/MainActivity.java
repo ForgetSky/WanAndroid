9fa5fe31369df75ac97e3d22d95904d069d2ff59
ForgetSky <275514805@qq.com>
2019-01-25 18:18:54 +0800

    add base packages

--- app/src/main/java/com/forgetsky/wanandroid/main/MainActivity.java
+++ app/src/main/java/com/forgetsky/wanandroid/main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.forgetsky.wanandroid;
+package com.forgetsky.wanandroid.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.forgetsky.wanandroid.R;
 
 public class MainActivity extends AppCompatActivity {
 
