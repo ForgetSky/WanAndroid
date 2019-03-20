0da738ff57805a96c319602e976333db84753057
ForgetSky <275514805@qq.com>
2019-03-20 18:15:52 +0800

    update splash activity

--- app/src/main/java/com/forgetsky/wanandroid/modules/main/ui/activity/SplashActivity.java
+++ app/src/main/java/com/forgetsky/wanandroid/modules/main/ui/activity/SplashActivity.java
@@ -3,17 +3,45 @@ package com.forgetsky.wanandroid.modules.main.ui.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+
+import com.forgetsky.wanandroid.R;
 
 /**
  * @author ForgetSky
  * @date 19-2-25
  */
 public class SplashActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashActivity.this, MainActivity.class));
-        finish();
+        setContentView(R.layout.activity_splash);
+
+        AlphaAnimation alphaAnimation = new AlphaAnimation(0.3F, 1.0F);
+        alphaAnimation.setDuration(1500);
+
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        findViewById(R.id.layout_splash).startAnimation(alphaAnimation);
     }
 
 }
